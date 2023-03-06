647f8bb7f11005f6001aba14bf57ddb46891501d
Junyao Shi <jackshi1999@gmail.com>
2023-03-06 17:05:50 -0500

    added direction to dijkstra state

--- lfhf/reward.py
+++ lfhf/reward.py
@@ -11,27 +11,64 @@ from minigrid.wrappers import FullyObsWrapper
 from minigrid.core.constants import OBJECT_TO_IDX
 
 
-def find_goal(world_grid):
+"""
+> The integer values to which the agent's direction is encoded are:
+> 
+> - `0`: East
+> - `1`: South
+> - `2`: West
+> - `3`: North
+
+> World grid encoding:
+world_grid[x_pos, y_pos] = [
+    OBJECT_TO_IDX[object_type], 
+    COLOR_TO_IDX[object_color], 
+    agent direction / objectstate
+]
+"""
+N_DIRS = 4
+EAST, SOUTH, WEST, NORTH = range(N_DIRS)
+WALL = OBJECT_TO_IDX["wall"]
+
+
+def find_goal_states(world_grid):
+    """Find the goal states in the world grid."""
     width, height, _ = world_grid.shape
+    goal_x, goal_y = None, None
     for i in range(width):
         for j in range(height):
             if world_grid[i, j, 0] == OBJECT_TO_IDX["goal"]:
-                return i, j
-    return None
+                goal_x, goal_y = i, j
+
+    assert goal_x is not None and goal_y is not None
+    goal_states = [(goal_x, goal_y, dir) for dir in range(N_DIRS)]
+    return goal_states
 
 
-def get_neighbors(world_grid, node):
+def get_neighbors(world_grid, state):
     width, height, _ = world_grid.shape
-    row, col = node
+    x, y, dir = state
     neighbors = []
-    if row > 0 and world_grid[row - 1, col, 0] != OBJECT_TO_IDX["wall"]:
-        neighbors.append((row - 1, col))
-    if row < width - 1 and world_grid[row + 1, col, 0] != OBJECT_TO_IDX["wall"]:
-        neighbors.append((row + 1, col))
-    if col > 0 and world_grid[row, col - 1, 0] != OBJECT_TO_IDX["wall"]:
-        neighbors.append((row, col - 1))
-    if col < height - 1 and world_grid[row, col + 1, 0] != OBJECT_TO_IDX["wall"]:
-        neighbors.append((row, col + 1))
+
+    # should not be at wall state (periphery of env)
+    assert x != 0 and y != 0 and x != width - 1 and y != height - 1
+
+    # turn left
+    neighbors.append((x, y, dir - 1 if dir > 0 else 3))
+
+    # turn right
+    neighbors.append((x, y, dir + 1 if dir < 3 else 0))
+
+    # move forward
+    if dir == EAST and world_grid[x + 1, y, 0] != WALL:
+        neighbors.append((x + 1, y, dir))
+    elif dir == SOUTH and world_grid[x, y + 1, 0] != WALL:
+        neighbors.append((x, y + 1, dir))
+    elif dir == WEST and world_grid[x - 1, y, 0] != WALL:
+        neighbors.append((x - 1, y, dir))
+    elif dir == NORTH and world_grid[x, y - 1, 0] != WALL:
+        neighbors.append((x, y - 1, dir))
+
     return neighbors
 
 
@@ -40,42 +77,57 @@ def dijkstra(world_grid):
     priority_queue = []
 
     width, height, _ = world_grid.shape
-    dist_to_goal = np.ones((width, height)) * np.inf
+    dist_to_goal = np.ones((width, height, N_DIRS)) * np.inf  # x, y, direction
 
-    goal = find_goal(world_grid)
-    assert goal is not None
-    goal_row, goal_col = goal
-    dist_to_goal[goal_row, goal_col] = 0
-    heapq.heappush(priority_queue, (0, (goal_row, goal_col)))
+    goal_states = find_goal_states(world_grid)
+    for goal_state in goal_states:
+        dist_to_goal[goal_state] = 0
+        heapq.heappush(priority_queue, (0, goal_state))
 
-    pbar = tqdm(desc="Dijkstra", total=width * height)
+    pbar = tqdm(desc="Dijkstra", total=width * height * N_DIRS)
     while priority_queue:
-        # go greedily by always extending the shorter cost nodes first
-        _, node = heapq.heappop(priority_queue)
-        visited.add(node)
+        # go greedily by always extending the shorter cost states first
+        _, state = heapq.heappop(priority_queue)
+        visited.add(state)
         pbar.update(1)
 
-        for neighbor in get_neighbors(world_grid, node):
+        for neighbor in get_neighbors(world_grid, state):
             if neighbor in visited:
                 continue
 
-            row, col = node
-            neighbor_row, neighbor_col = neighbor
-            new_dist = dist_to_goal[row, col] + 1
-            if new_dist < dist_to_goal[neighbor_row, neighbor_col]:
-                dist_to_goal[neighbor_row, neighbor_col] = new_dist
-                heapq.heappush(
-                    priority_queue, (dist_to_goal[neighbor_row, neighbor_col], neighbor)
-                )
+            new_dist = dist_to_goal[state] + 1
+            if new_dist < dist_to_goal[neighbor]:
+                dist_to_goal[neighbor] = new_dist
+                heapq.heappush(priority_queue, (dist_to_goal[neighbor], neighbor))
 
     pbar.close()
     return dist_to_goal
+
+
+def print_world_grid(world_grid):
+    """Prints the world grid in a readable format.
+    Using transpose because np array's indexing is [row, col] and we want [x, y].
+    """
+    print("World grid:")
+    pprint(np.transpose(world_grid[:, :, 0]))
+
+
+def print_dist_to_goal(dist_to_goal):
+    """Prints the distance to goal in a readable format.
+    Using transpose because np array's indexing is [row, col] and we want [x, y].
+    """
+    print("Distance to goal:")
+    pprint(np.transpose(np.mean(dist_to_goal, axis=2).round(1)))
 
 
 if __name__ == "__main__":
     np.set_printoptions(linewidth=200)
 
     env_name = "MiniGrid-FourRooms-v0"
+    # env_name = "MiniGrid-Empty-5x5-v0"
+    # env_name = "MiniGrid-Empty-8x8-v0"
+    # env_name = "MiniGrid-Empty-Random-5x5-v0"
+
     env: MiniGridEnv = gym.make(env_name, render_mode="rgb_array")
     env = FullyObsWrapper(env)
     obs, _ = env.reset()
@@ -84,8 +136,7 @@ if __name__ == "__main__":
     img.save("obs.jpg")
 
     world_grid = obs["image"]
-    print("World grid:")
-    pprint(np.transpose(world_grid[:, :, 0]))
+    print_world_grid(world_grid)
 
     # obs, *_ = env.step(env.actions.left)
     # world_grid = obs["image"]
@@ -97,6 +148,5 @@ if __name__ == "__main__":
     # pprint('World grid after moving forward:')
     # pprint(world_grid[:, :, 0])
 
-    # dist_to_goal = dijkstra(world_grid)
-    # pprint("Distance to goal:")
-    # pprint(dist_to_goal)
+    dist_to_goal = dijkstra(world_grid)
+    print_dist_to_goal(dist_to_goal)
