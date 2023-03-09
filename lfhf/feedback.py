b6399aeca612562de7afd853356805c6efd5af81
Junyao Shi <jackshi1999@gmail.com>
2023-03-09 00:00:33 -0500

    added noise param to feedback func

--- lfhf/feedback.py
+++ lfhf/feedback.py
@@ -1,16 +1,19 @@
 import numpy as np
 
-def noisy_sigmoid_feedback(value):
-    return 1/(1 + np.exp(-value)) + np.random.normal(0, 0.05)
+
+def noisy_sigmoid_feedback(value, noise=0.05):
+    if isinstance(value, np.ndarray):  # vectorized
+        return 1 / (1 + np.exp(-value)) + np.random.normal(0, noise, value.shape)
+    else:  # scalar
+        return 1 / (1 + np.exp(-value)) + np.random.normal(0, noise)
 
 
 if __name__ == "__main__":
     # Use 25 1s and 25 -1s, plot the noisy sigmoid feedback
     import matplotlib.pyplot as plt
-    vals = np.ones(50)
-    vals[:25] *= -1
-    feedback = []
-    for val in vals:
-        feedback.append(noisy_sigmoid_feedback(val))
+
+    vals = np.ones(1000)
+    vals[:500] *= -1
+    feedback = noisy_sigmoid_feedback(vals)
     plt.scatter(vals, feedback, s=2)
-    plt.savefig('feedback.png')
+    plt.savefig("feedback.png")
