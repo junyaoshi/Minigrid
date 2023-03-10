82b4a25ac75ddbc69a871c930b1bc5329663d51d
Junyao Shi <jackshi1999@gmail.com>
2023-03-10 11:49:04 -0500

    reformatted code

--- lfhf/qlearning.py
+++ lfhf/qlearning.py
@@ -1,5 +1,4 @@
 from pprint import pprint
-import heapq
 from PIL import Image
 
 import numpy as np
@@ -13,7 +12,8 @@ from minigrid.utils.window import Window
 
 epsilon = 0.05
 alpha = 0.1
-gamma = 1
+gamma = 0.99
+
 
 def find_goal(world_grid):
     width, height, _ = world_grid.shape
@@ -23,6 +23,7 @@ def find_goal(world_grid):
                 return i, j
     return None
 
+
 def find_agent(world_grid):
     width, height, _ = world_grid.shape
     for i in range(width):
@@ -31,25 +32,31 @@ def find_agent(world_grid):
                 return i, j, world_grid[i, j, 2]
     return None
 
+
 def choose_action(q_table, world_grid, available_actions, epsilon):
     """Returns the optimal action from Q-Value table. If multiple optimal actions, chooses random choice.
     Will make an exploratory random action dependent on epsilon."""
-    if np.random.uniform(0,1) < epsilon:
+    if np.random.uniform(0, 1) < epsilon:
         action = available_actions[np.random.randint(0, len(available_actions))]
     else:
         q_values_of_state = q_table[find_agent(world_grid)]
         maxValue = max(q_values_of_state.values())
-        action = np.random.choice([k for k, v in q_values_of_state.items() if v == maxValue])
+        action = np.random.choice(
+            [k for k, v in q_values_of_state.items() if v == maxValue]
+        )
 
     return action
+
 
 def learn(q_table, old_state, reward, new_state, action):
     """Updates the Q-value table using Q-learning"""
     q_values_of_state = q_table[new_state]
     max_q_value_in_new_state = max(q_values_of_state.values())
     current_q_value = q_table[old_state][action]
-    
-    q_table[old_state][action] = (1 - alpha) * current_q_value + alpha * (reward + gamma * max_q_value_in_new_state)
+
+    q_table[old_state][action] = (1 - alpha) * current_q_value + alpha * (
+        reward + gamma * max_q_value_in_new_state
+    )
 
 
 if __name__ == "__main__":
@@ -73,10 +80,13 @@ if __name__ == "__main__":
     for x in range(env.width):
         for y in range(env.height):
             for dir in range(4):
-                q_table[(x, y, dir)] = {env.actions.left:0, env.actions.right:0, env.actions.forward:0}
+                q_table[(x, y, dir)] = {
+                    env.actions.left: 0,
+                    env.actions.right: 0,
+                    env.actions.forward: 0,
+                }
     available_actions = [env.actions.left, env.actions.right, env.actions.forward]
-    
-    
+
     num_trials = 1000
     reward_per_episode = []
     for trial in range(num_trials):
@@ -118,8 +128,8 @@ if __name__ == "__main__":
     # img = env.render()
     # img = Image.fromarray(img)
     # img.save("obs.jpg")
-    for x in range(1, env.width-1):
-        for y in range(1, env.height-1):
+    for x in range(1, env.width - 1):
+        for y in range(1, env.height - 1):
             for dir in range(4):
                 print(x, y, dir, q_table[(x, y, dir)])
-    print('done')
+    print("done")
