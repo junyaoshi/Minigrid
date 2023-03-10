94437ad2960cd30663b2e39aa8cc76fdb5d814f7
Junyao Shi <jackshi1999@gmail.com>
2023-03-10 11:34:46 -0500

    fixed some bugs and implemented model training

--- lfhf/ground_reward.py
+++ lfhf/ground_reward.py
@@ -1,36 +1,48 @@
 import argparse
+from pprint import pprint
 
 from PIL import Image
 import numpy as np
 import gymnasium as gym
+from torch.utils.tensorboard import SummaryWriter
 
 from minigrid.minigrid_env import MiniGridEnv
 from minigrid.wrappers import FullyObsWrapper
 
 from feedback import noisy_sigmoid_feedback
 from behavior import random_action, compute_coverage
-from reward import dijkstra, compute_reward
-from utils import print_world_grid, print_dist_to_goal
+from reward import dijkstra, compute_reward, get_all_reward_samples
+from utils import print_world_grid, print_dist_to_goal, kl_divergence, str2bool
+from model import train_model
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("--log_dir", type=str, default="results/test")
+
+    # probe env
     parser.add_argument("--probe_env", type=str, default="MiniGrid-Empty-5x5-v0")
     parser.add_argument("--n_probe_samples", type=int, default=100)
+
+    # feedback
     parser.add_argument("--noise", type=float, default=0.05)
 
-    return parser.parse_args()
+    # model training
+    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--lr", type=float, default=3e-4)
+    parser.add_argument("--n_epochs", type=int, default=50)
+    parser.add_argument(
+        "--n_blocks", type=int, default=2, help="number of network blocks"
+    )
+    parser.add_argument("--residual", type=str2bool, default=True)
+
+    args = parser.parse_args()
+    return args
 
 
-def generate_samples(env: MiniGridEnv, args):
-    # dijkstra distance to goal
-    obs, _ = env.reset()
-    world_grid = obs["image"]
-    dist_to_goal = dijkstra(world_grid)
-    print_dist_to_goal(dist_to_goal)
-
-    # generate samples
+def generate_samples(env: MiniGridEnv, args, world_grid, dist_to_goal):
+    """Generate reward samples from the environment."""
     visited = set()
     reward_data = []
     for _ in range(args.n_probe_samples):
@@ -42,23 +54,26 @@ def generate_samples(env: MiniGridEnv, args):
         *_, terminated, truncated, _ = env.step(action)
         next_state = (*env.agent_pos, env.agent_dir)
         reward = compute_reward(dist_to_goal, state, next_state)
-        if reward not in [-1, 0, 1]:
-            print()
         reward_data.append(reward)
 
         if terminated or truncated:
             env.reset()
 
     reward_data = np.array(reward_data)
-    print(reward_data)
-    feedback_data = noisy_sigmoid_feedback(reward_data, args.noise)
+    coverage = compute_coverage(world_grid, visited)
 
-    print(f"Coverage: {compute_coverage(world_grid, visited):.4f}")
-
-    return reward_data, feedback_data
+    return reward_data, coverage
 
 
 def main(args):
+    writer = SummaryWriter(args.log_dir)
+
+    # log args
+    pprint(f"args: \n{args}")
+    for arg in vars(args):
+        writer.add_text(f"args/{str(arg)}", str(getattr(args, arg)))
+    writer = writer
+
     env: MiniGridEnv = gym.make(args.probe_env, render_mode="rgb_array")
     env = FullyObsWrapper(env)
     obs, _ = env.reset()
@@ -68,9 +83,38 @@ def main(args):
     print_world_grid(world_grid)
     Image.fromarray(env.render()).save("obs.jpg")
 
-    reward_data, feedback_data = generate_samples(env, args)
+    # generate reward and feedback samples and ground truth
+    dist_to_goal = dijkstra(world_grid)
+    print_dist_to_goal(dist_to_goal)
+
+    all_reward = get_all_reward_samples(args.probe_env, dist_to_goal)
+    np.random.shuffle(all_reward)
+    all_feedback = noisy_sigmoid_feedback(all_reward, args.noise)
+
+    reward_data, coverage = generate_samples(env, args, world_grid, dist_to_goal)
+    np.random.shuffle(reward_data)
+    feedback_data = noisy_sigmoid_feedback(reward_data, args.noise)
+
+    kl_div = kl_divergence(reward_data, all_reward)
+    print(f"State x Action space coverage: {coverage:.4f}")
+    print(f"KL Divergence between reward samples and ground truth: {kl_div:.4f}")
+
+    # tensorboard logging
+    writer.add_histogram("reward/all_reward", all_reward)
+    writer.add_histogram("reward/reward_data", reward_data)
+    writer.add_histogram("feedback/feedback_data", feedback_data)
+    writer.add_histogram("feedback/all_feedback", all_feedback)
+    writer.add_text("stats/coverage", str(coverage))
+    writer.add_text("stats/reward_kl_div", str(kl_div))
+
+    model = train_model(
+        reward_data, feedback_data, all_reward, all_feedback, args, writer
+    )
 
 
 if __name__ == "__main__":
     args = parse_args()
     main(args)
+    from time import sleep
+
+    sleep(0.5)
