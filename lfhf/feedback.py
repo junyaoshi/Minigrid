15142ff8c377dce7fe2a67978596a51f8d74cef2
Junyao Shi <jackshi1999@gmail.com>
2023-03-20 16:34:15 -0400

    begin implementation of base 2 feedback

--- lfhf/feedback.py
+++ lfhf/feedback.py
@@ -6,14 +6,22 @@ def noisy_sigmoid_feedback(value, noise=0.05):
         return 1 / (1 + np.exp(-value)) + np.random.normal(0, noise, value.shape)
     else:  # scalar
         return 1 / (1 + np.exp(-value)) + np.random.normal(0, noise)
+    
+
+def noisy_base2_feedback(value, noise=0.05):
+    # convert base 10 to base 2
+    pass
 
 
 if __name__ == "__main__":
-    # Use 25 1s and 25 -1s, plot the noisy sigmoid feedback
     import matplotlib.pyplot as plt
 
-    vals = np.ones(1000)
-    vals[:500] *= -1
-    feedback = noisy_sigmoid_feedback(vals)
-    plt.scatter(vals, feedback, s=2)
-    plt.savefig("feedback.png")
+    test_sigmoid = True
+
+    # Use 25 1s and 25 -1s, plot the noisy sigmoid feedback
+    if test_sigmoid:
+        vals = np.ones(1000)
+        vals[:500] *= -1
+        feedback = noisy_sigmoid_feedback(vals)
+        plt.scatter(vals, feedback, s=2)
+        plt.savefig("feedback.png")
